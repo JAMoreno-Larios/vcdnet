b1c9645155fafe3dd91d50e2d665c90b98c2313f
JAMoreno-Larios <agustin90m@gmail.com>
2022-02-16 11:49:20 -0500

    Added CoordConv implementation to the U-Net models

--- model/unet.py
+++ model/unet.py
@@ -6,6 +6,50 @@ __all__ = ['UNet_A',
            'UNet_B',
            'UNet_C',
            ]
+
+def add_coords_layers(input_tensor):
+	"""
+	For a given tensor, add additional layers specifying the xy coordinates. 
+
+	Args:
+		input_tensor: An input tensor not containining coordinate layers. 
+	Returns:
+		output_tensor: Similar to input tensor but with two additional layers
+			specifying xy coordinates. 
+    Idea sourced from: https://github.com/uber-research/CoordConv
+	"""
+	batch_size_tensor = tf.shape(input_tensor)[0]
+	x_dim = tf.shape(input_tensor)[1]
+	y_dim = tf.shape(input_tensor)[2]
+	xx_ones = tf.ones([batch_size_tensor, x_dim], dtype=tf.int32)
+	xx_ones = tf.expand_dims(xx_ones, -1)
+	xx_range = tf.tile( 
+		tf.expand_dims(tf.range(y_dim), 0), [batch_size_tensor, 1])
+	xx_range = tf.expand_dims(xx_range, 1)
+	xx_channel = tf.matmul(xx_ones, xx_range)
+	xx_channel = tf.expand_dims(xx_channel, -1)
+	yy_ones = tf.ones([batch_size_tensor, y_dim], dtype=tf.int32)
+	yy_ones = tf.expand_dims(yy_ones, 1)
+	yy_range = tf.tile(
+		tf.expand_dims(tf.range(x_dim), 0), [batch_size_tensor, 1])
+	yy_range = tf.expand_dims(yy_range, -1)
+	yy_channel = tf.matmul(yy_range, yy_ones)
+	yy_channel = tf.expand_dims(yy_channel, -1)
+
+	x_dim = tf.cast(x_dim, tf.float32)
+	y_dim = tf.cast(y_dim, tf.float32)
+
+	xx_channel = tf.cast(xx_channel, tf.float32) / (x_dim - 1)
+	yy_channel = tf.cast(yy_channel, tf.float32) / (y_dim - 1)
+	xx_channel = xx_channel*2 - 1
+	yy_channel = yy_channel*2 - 1
+
+	ret = tf.concat([input_tensor, xx_channel, yy_channel], axis=-1)
+	output_tensor = ret
+	return output_tensor
+
+
+
 def upscale(layer, out_channels, out_size=None, scale=None, mode='upconv', name='upsale'):
     # if (out_size is None) and (scale is None):
     #     raise ValueError("At least one of out_size and scale must be non-None")
@@ -55,6 +99,7 @@ def UNet_A(lf_extra, n_slices, output_size, is_train=True, reuse=False, name='un
     act = tf.nn.relu
 
     with tf.variable_scope(name, reuse=reuse):
+        lf_extra = add_coords_layers(lf_extra) # hardcode coordinates to tensor
         n = InputLayer(lf_extra, 'lf_extra')
         n = conv2d(n, n_filter=channels_interp, filter_size=7, name='conv1')
 
@@ -147,6 +192,7 @@ def UNet_B(lf_extra, n_slices, output_size,
     act = tf.nn.relu
 
     with tf.variable_scope(name, reuse=reuse):
+        lf_extra = add_coords_layers(lf_extra) # hardcode coordinates to tensor
         n = InputLayer(lf_extra, 'lf_extra')
         n = conv2d(n, n_filter=channels_interp, filter_size=5, name='conv1')
 
@@ -239,6 +285,7 @@ def UNet_C(lf_extra, n_slices, output_size, is_train=True, reuse=False, name='un
     n_interp = 4
     channels_interp=128
     with tf.variable_scope(name, reuse=reuse):
+        lf_extra = add_coords_layers(lf_extra) # hardcode coordinates to tensor
         n = InputLayer(lf_extra, 'lf_extra')
         n = conv2d(n, n_filter=channels_interp, filter_size=7, name='conv1')
         with tf.variable_scope('interp'):
